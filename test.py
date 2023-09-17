7cee9518f6ef9619b65d1b25af8444c859dac159
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2023-09-17 16:12:34 -0700

    Update test.py

--- test.py
+++ test.py
@@ -456,6 +456,8 @@ def Manage_New_Property_Mapping(operator):
                 st.write(entity_mapping_update.loc[i,"Property_Name"])
             with col2:
                 entity_mapping_update.loc[i,"Sheet_Name"]=st.text_input("",placeholder =entity_mapping.loc[i,"Sheet_Name"],key="P&L"+str(i))    
+                if not entity_mapping_update.loc[i,"Sheet_Name"]:
+                    entity_mapping_update.loc[i,"Sheet_Name"]=entity_mapping.loc[i,"Sheet_Name"]                 	
             with col3: 
                 entity_mapping_update.loc[i,"Sheet_Name_Occupancy"]=st.text_input("",placeholder =entity_mapping.loc[i,"Sheet_Name_Occupancy"],key="Census"+str(i))    
             with col4:
