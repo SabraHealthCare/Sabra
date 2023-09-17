7bfa038099ab3b52736fb687c67a2504d31abb45
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2023-09-17 16:39:28 -0700

    Update test.py

--- test.py
+++ test.py
@@ -466,8 +466,8 @@ def Manage_New_Property_Mapping(operator):
                 entity_mapping.loc[i,"Sheet_Name_Occupancy"]=entity_mapping_updation.loc[i,"Sheet_Name_Occupancy"]
             if not entity_mapping_updation.loc[i,"Sheet_Name_Balance_Sheet"]:
                 entity_mapping.loc[i,"Sheet_Name_Balance_Sheet"]=entity_mapping_updation.loc[i,"Sheet_Name_Balance_Sheet"] 
-        st.write(entity_mapping)
-        download_report(entity_mapping_update,"{} properties mapping".format(operator))
+        st.write(entity_mapping["Property_Name","Sheet_Name","Sheet_Name_Occupancy","Sheet_Name_Balance_Sheet"])
+        download_report(entity_mapping["Property_Name","Sheet_Name","Sheet_Name_Occupancy","Sheet_Name_Balance_Sheet"],"{} properties mapping".format(operator))
        
         return entity_mapping
     else:
