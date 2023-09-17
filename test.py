640659477e5ecb15967f5dac228effd2238d0af9
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2023-09-16 18:50:26 -0700

    Update test.py

--- test.py
+++ test.py
@@ -442,13 +442,13 @@ def Manage_New_Property_Mapping():
     with st.form(key="Mapping Properties"):
         col1,col2,col3,col4=st.columns(4)
         with col1:
-                st.write("Property Name")
-            with col2:
-                st.write("Sheetname of P&L")    
-            with col3: 
-                st.write("Sheetname of Census")    
-            with col4:
-                st.write("Sheetname of Balance sheet")  
+            st.write("Property Name")
+        with col2
+            st.write("Sheetname of P&L")    
+        with col3: 
+            st.write("Sheetname of Census")    
+        with col4:
+            st.write("Sheetname of Balance sheet")  
         for i in range(entity_mapping.shape[0]):
             col1,col2,col3,col4=st.columns(4)
             with col1:
