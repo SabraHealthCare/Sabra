be61ee774517f5a3b7a7420d378cb18d67eb082d
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2023-09-19 14:51:25 -0700

    Update test.py

--- test.py
+++ test.py
@@ -590,14 +590,14 @@ def Sheet_Process(entity_i,sheet_type,sheet_name):
 def Mapping_PL_Sabra(PL,entity):
     # remove no need to map from account_mapping
     main_account_mapping=account_mapping.loc[list(map(lambda x:x==x and x.upper()!='NO NEED TO MAP',account_mapping["Sabra_Account"])),:]
-    st.write(main_account_mapping)
+    #st.write(main_account_mapping)
     #concat main accounts with second accounts
     second_account_mapping=account_mapping.loc[(account_mapping["Sabra_Second_Account"]==account_mapping["Sabra_Second_Account"])&(account_mapping["Sabra_Second_Account"]!="NO NEED TO MAP")][["Sabra_Second_Account","Tenant_Formated_Account","Tenant_Account","Conversion"]].\
                            rename(columns={"Sabra_Second_Account": "Sabra_Account"})
-    st.write(second_account_mapping)
+    #st.write(second_account_mapping)
     PL.index.name="Tenant_Account"
     PL["Tenant_Formated_Account"]=list(map(lambda x:x.upper() if type(x)==str else x,PL.index))
-    st.write(PL)
+    #st.write(PL)
     PL=pd.concat([PL.merge(second_account_mapping,on="Tenant_Formated_Account",how='right'),PL.merge(main_account_mapping[main_account_mapping["Sabra_Account"]==main_account_mapping["Sabra_Account"]]\
                                             [["Sabra_Account","Tenant_Formated_Account","Tenant_Account","Conversion"]],on="Tenant_Formated_Account",how='right')])
     st.write(PL)
@@ -794,6 +794,7 @@ def PL_Process_Main(entity_i,sheet_type):
     sheet_name=str(entity_mapping.loc[entity_i,sheet_type])
     if True:
             PL=Sheet_Process(entity_i,sheet_type,sheet_name)
+            st.write(PL)
             # mapping new tenant accounts
             new_tenant_account_list=list(filter(lambda x:x.upper().strip() not in list(account_mapping["Tenant_Formated_Account"]),PL.index))
             
