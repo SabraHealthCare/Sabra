77ecaf0caf00692f0f25575a9f992193ee880f31
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2023-09-20 16:03:45 -0700

    Update test.py

--- test.py
+++ test.py
@@ -151,7 +151,7 @@ def Identify_Tenant_Account_Col(PL,sheet_name,sheet_type):
     #search tenant account column in P&L, return col number of tenant account
     account_pool=account_mapping[["Sabra_Account","Tenant_Formated_Account"]].merge(BPC_Account[["BPC_Account_Name","Category"]], left_on="Sabra_Account", right_on="BPC_Account_Name",how="left")	       
     if sheet_type=="Sheet_Name_Occupancy": 
-	account_pool=account_pool.loc[account_pool["Category"]=="Patient Days"]["Tenant_Formated_Account"]	       
+        account_pool=account_pool.loc[account_pool["Category"]=="Patient Days"]["Tenant_Formated_Account"]	       
     elif sheet_type=="Sheet_Name_Balance_Sheet":
         account_pool=account_pool.loc[account_pool["Category"]=="Balance Sheet"]["Tenant_Formated_Account"]
     st.write(account_pool)
