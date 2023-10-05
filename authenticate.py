228f607b082d57b3f923d1c4264cabe00c1dc25b
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2023-10-05 11:41:24 -0700

    Update authenticate.py

--- authenticate.py
+++ authenticate.py
@@ -2,8 +2,14 @@ import pandas as pd  # pip install pandas openpyxl
 import plotly.express as px  # pip install plotly-express
 import streamlit as st  # pip install streamlit
 import streamlit_authenticator as stauth  # pip install streamlit-authenticator
+conn = st.experimental_connection('mysql', type='sql')
 
-import database as db
+# Perform query.
+df = conn.query('SELECT * from users;', ttl=600)
+
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.user_name} has a :{row.password}:")
 
 
 # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
