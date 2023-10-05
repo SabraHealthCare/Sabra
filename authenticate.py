3240c195505b7ebc27deb873d92980b03cab212a
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2023-10-05 14:17:16 -0700

    Update authenticate.py

--- authenticate.py
+++ authenticate.py
@@ -12,5 +12,5 @@ for row in df.itertuples():
     st.write(f"{row.user_name} has a :{row.password}:")
 
 # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
-st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
+#st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
 
