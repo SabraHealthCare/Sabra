2eb9116da6bdacf8a191bdd34150c714f605a99c
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2023-10-05 19:03:22 -0700

    Update and rename Setup.py to generate_keys.py

---
+++ generate_keys.py
@@ -0,0 +1,16 @@
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords=['Python', 'Streamlit', 'Authentication', 'Components'],
+    python_requires=">=3.6",
+    install_requires=[
+        "PyJWT >=2.3.0",
+        "bcrypt >= 3.1.7",
+        "PyYAML >= 5.3.1",
+        "streamlit >= 1.18.0",
+        "extra-streamlit-components >= 0.1.60"
+    ],
+)
