32e27881a93c53e7867535831f08da6c2ffce0c7
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2023-10-06 09:52:45 -0700

    Update authenticate.py

--- authenticate.py
+++ authenticate.py
@@ -1,6 +1,9 @@
 #import pickle
 #from pathlib import Path
-
+import pandas as pd  
+#import plotly.express as px  # pip install plotly-express
+import streamlit as st  
+import streamlit_authenticator as stauth 
  
 st.write(2)
 
