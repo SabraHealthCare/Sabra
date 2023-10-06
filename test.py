434fccfc8d9311a134e2d8717d70de8a7d8bdb1b
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2023-10-06 14:52:12 -0700

    Create test.py

---
+++ test.py
@@ -0,0 +1,82 @@
+# --- DEMO PURPOSE ONLY --- #
+placeholder = st.empty()
+placeholder.info("CREDENTIALS | username:pparker ; password:abc123")
+# ------------------------- #
+
+# --- USER AUTHENTICATION ---
+users = db.fetch_all_users()
+
+usernames = [user["key"] for user in users]
+names = [user["name"] for user in users]
+hashed_passwords = [user["password"] for user in users]
+
+authenticator = stauth.Authenticate(credentials, "some_cookie_name", "some_signature_key", cookie_expiry_days=30)
+name, authentication_status, username = authenticator.login("Login", "main")
+
+if authentication_status == False:
+    st.error("Username/password is incorrect")
+
+if authentication_status == None:
+    st.warning("Please enter your username and password")
+      
+
+if authentication_status == False:
+    st.error("Username/password is incorrect")
+    st.write(name, authentication_status, username)
+
+if authentication_status == None:
+    st.warning("Please enter your username and password")
+    st.write(name, authentication_status, username)
+if authentication_status:
+    placeholder.empty()
+    st.write("got")
+
+   
+    # ---- SIDEBAR ----
+    authenticator.logout("Logout", "sidebar")
+    st.sidebar.title(f"Welcome {name}")
+    st.sidebar.header("Please Filter Here:")
+    city = st.sidebar.multiselect(
+        "Select the City:",
+        options=df["City"].unique(),
+        default=df["City"].unique()
+    )
+
+    customer_type = st.sidebar.multiselect(
+        "Select the Customer Type:",
+        options=df["Customer_type"].unique(),
+        default=df["Customer_type"].unique(),
+    )
+
+    gender = st.sidebar.multiselect(
+        "Select the Gender:",
+        options=df["Gender"].unique(),
+        default=df["Gender"].unique()
+    )
+
+    df_selection = df.query(
+        "City == @city & Customer_type ==@customer_type & Gender == @gender"
+    )
+
+    # ---- MAINPAGE ----
+    st.title(":bar_chart: Sales Dashboard")
+    st.markdown("##")
+
+    # TOP KPI's
+    total_sales = int(df_selection["Total"].sum())
+    average_rating = round(df_selection["Rating"].mean(), 1)
+    star_rating = ":star:" * int(round(average_rating, 0))
+    average_sale_by_transaction = round(df_selection["Total"].mean(), 2)
+
+    left_column, middle_column, right_column = st.columns(3)
+    with left_column:
+        st.subheader("Total Sales:")
+        st.subheader(f"US $ {total_sales:,}")
+    with middle_column:
+        st.subheader("Average Rating:")
+        st.subheader(f"{average_rating} {star_rating}")
+    with right_column:
+        st.subheader("Average Sales Per Transaction:")
+        st.subheader(f"US $ {average_sale_by_transaction}")
+
+    st.markdown("""-------------------------------------------------""")
