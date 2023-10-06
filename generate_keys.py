4e0e4ffff9fae8234b5f00b51e3578a10f665c18
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2023-10-05 19:04:58 -0700

    Update generate_keys.py

--- generate_keys.py
+++ generate_keys.py
@@ -1,16 +1,14 @@
+import pickle
+from pathlib import Path
 
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    keywords=['Python', 'Streamlit', 'Authentication', 'Components'],
-    python_requires=">=3.6",
-    install_requires=[
-        "PyJWT >=2.3.0",
-        "bcrypt >= 3.1.7",
-        "PyYAML >= 5.3.1",
-        "streamlit >= 1.18.0",
-        "extra-streamlit-components >= 0.1.60"
-    ],
-)
+import streamlit_authenticator as stauth
+
+names = ["Peter Parker", "Rebecca Miller"]
+usernames = ["pparker", "rmiller"]
+passwords = ["XXX", "XXX"]
+
+hashed_passwords = stauth.Hasher(passwords).generate()
+
+file_path = Path(__file__).parent / "hashed_pw.pkl"
+with file_path.open("wb") as file:
+    pickle.dump(hashed_passwords, file)
