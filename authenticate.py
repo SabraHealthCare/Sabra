dd1759cae20b843ff1709b5167d36b5bde02c3cd
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2023-10-06 10:27:42 -0700

    Update authenticate.py

--- authenticate.py
+++ authenticate.py
@@ -5,17 +5,9 @@ import pandas as pd
 import streamlit as st  
 
 import streamlit_authenticator as stauth 
-usernames = ['user1','user2']
-names = ['name1','name2']
-passwords = ['pwd1','pwd2']
 
-credentials = {"usernames":{}}
-        
-for uname,name,pwd in zip(usernames,names,passwords):
-    user_dict = {"name": name, "password": pwd}
-    credentials["usernames"].update({uname: user_dict})
-        
-authenticator = stauth.Authenticate(credentials, "cokkie_name", "random_key", cookie_expiry_days=30)
+
+
 
 
 # --- USER AUTHENTICATION ---
@@ -27,7 +19,13 @@ file_path = Path(__file__).parent / "hashed_pw.pkl"
 with file_path.open("rb") as file:
     hashed_passwords = pickle.load(file)
 
-
+credentials = {"usernames":{}}
+        
+for uname,name,pwd in zip(usernames,names,passwords):
+    user_dict = {"name": name, "password": pwd}
+    credentials["usernames"].update({uname: user_dict})
+        
+authenticator = stauth.Authenticate(credentials, "cokkie_name", "random_key", cookie_expiry_days=30)
 name, authentication_status, username = authenticator.login("Login", "main")
 
 if authentication_status == False:
