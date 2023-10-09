e10dbcef8ca68d9ebf04c7cb1ca7dc8acdd81c1b
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2023-10-08 17:51:16 -0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -59,7 +59,7 @@ if _RELEASE:
         username_forgot_pw, email_forgot_password, random_password = authenticator.forgot_password('Forgot password')
         if username_forgot_pw:
             st.success('New password sent securely')
-            st.write(username_forgot_pw)
+            st.write(username_forgot_pw,email_forgot_password,random_password)
             # Random password to be transferred to user securely
         else:
             st.error('Username not found')
@@ -71,7 +71,7 @@ if _RELEASE:
         username_forgot_username, email_forgot_username = authenticator.forgot_username('Forgot username')
         if username_forgot_username:
             st.success('Username sent securely')
-            st.write(username_forgot_username)
+            st.write(username_forgot_username, email_forgot_username)
             # Username to be transferred to user securely
         else:
             st.error('Email not found')
